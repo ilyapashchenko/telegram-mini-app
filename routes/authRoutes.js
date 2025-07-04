618b562f06d00832daba389895f6cd64ade00309
ilyapashchenko <Ilyapashchenkotest@gmail.com>
2025-07-04 20:57:30 +0300

    работаю над полем ввода

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,41 @@
 const express = require('express');
-const { authHandler, addPlaceById } = require('../controllers/authController');
-
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-router.post('/auth', authHandler);
-router.post('/addPlaceById', addPlaceById);
+router.post('/auth', authController.authHandler);
+router.post('/addPlaceById', authController.addPlaceById);
 router.post('/deletePlace', authController.deletePlace);
 
-
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const { authHandler, addPlaceById } = require('../controllers/authController');
+
+// const router = express.Router();
+
+// router.post('/auth', authHandler);
+// router.post('/addPlaceById', addPlaceById);
+
+
+
+// module.exports = router;
