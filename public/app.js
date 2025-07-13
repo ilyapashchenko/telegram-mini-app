72f88e2ddbfbb0cf03d3391bd1fd2922d19bfe29
ilyapashchenko <Ilyapashchenkotest@gmail.com>
2025-07-13 16:38:50 +0300

    доделываю

--- public/app.js
+++ public/app.js
@@ -756,6 +756,14 @@ function loadBookings() {
 
 
 
+function formatDate(dateStr) {
+  const date = new Date(dateStr);
+  return date.toLocaleDateString('ru-RU', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  });
+}
 
 
 
